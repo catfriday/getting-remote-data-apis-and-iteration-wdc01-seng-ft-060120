37556ed972aa6052f8a854fefbd9f1fead0a2a5b
Diwo777 <diego.zegarra79@icloud.com>
2020-06-15 14:58:37 -0400

    updated

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
@@ -7,5 +8,3 @@ welcome
 character = get_character_from_user
 show_character_movies(character)
 
-#  print get_character_movies_from_api("Luke Skywalker")
-print print_movies(film)
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,6 +1,7 @@
 require 'rest-client'
 require 'json'
 require 'pry'
+require 'pry-byebug'
 
 def get_character_movies_from_api(character_name)
   #make the web request
@@ -8,26 +9,28 @@ def get_character_movies_from_api(character_name)
   response_string = RestClient.get('http://swapi.dev/api/people')
   response_hash = JSON.parse(response_string)
   
-  films = response_hash["results"][0]["films"]
-  array_hash = []
-  films.each  do |film|
-   x = RestClient.get(film) 
-   x_hash = JSON.parse(x)
-    array_hash << x_hash 
-   
-  end
-  array_hash
- 
+  
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
+  character_hash = response_hash["results"].find do |character|
+    character["name"].downcase == character_name
+  end
+# collect those film API urls, make a web request to each URL to get the info
   #  for that film
+film_info = character_hash["films"].map do |film|
+    res_string = RestClient.get(film)
+    res_hash = JSON.parse(res_string)
+    
+  end
   # return value of this method should be collection of info about each film.
+  film_info
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
 end
+
+
 
 
 def print_movies(films)
@@ -39,7 +42,6 @@ def print_movies(films)
     puts film["release_date"]
     puts film["opening_crawl"]
   end
-  
 end
 
 def show_character_movies(character)
